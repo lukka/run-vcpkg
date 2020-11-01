bacfbc927091c5c738334c2c594ee478648b79da
luca <681992+lukka@users.noreply.github.com>
2020-11-01 17:47:14 +0100

    consume 1.0.x of run-vcpkg-cmake-action-libs packages

--- __tests__/basic.test.ts
+++ __tests__/basic.test.ts
@@ -1,12 +1,19 @@
 import * as process from 'process'
-import * as path from 'path'
-import * as io from '@actions/io'
 import * as fs from 'fs'
-import * as action from '../src/vcpkg-action'
-import * as postAction from '../src/post-action'
+import * as vcpkgAction from '../src/vcpkg-action'
+import * as actionLib from '@lukka/action-lib'
+import * as vcpkgUtils from '../src/vcpkg-utils'
+import * as cache from '@actions/cache'
+import * as core from '@actions/core'
+import * as runVcpkgLib from '@lukka/run-vcpkg-lib'
+import * as path from 'path'
+import { BaseUtilLib } from '@lukka/base-util-lib'
+import { ActionLib } from '@lukka/action-lib'
 
 jest.setTimeout(15 * 1000)
-//jest.mock('./action-lib');
+// Mocks entire action-lib module.
+jest.mock("@lukka/action-lib");
+jest.mock("@lukka/run-vcpkg-lib");
 
 function createDir(path: string) {
     try {
@@ -26,42 +33,145 @@ function clearInputs(): void {
         });
 }
 
-describe('basic run-vcpkg tests', () => {
-    beforeAll(async () => {
-        //jest.spyOn();
-        /*jest.spyOn(actionUtils, "isExactKeyMatch").mockImplementation(
-            (key, cacheResult) => {
-                const actualUtils = jest.requireActual("../src/utils/actionUtils");
-                return actualUtils.isExactKeyMatch(key, cacheResult);
+beforeAll(async () => {
+    process.env.GITHUB_WORKSPACE = "/var/tmp/";
+    jest.resetAllMocks();
+});
+
+beforeEach(() => {
+});
+
+afterEach(() => {
+    clearInputs();
+});
+
+afterAll(async () => {
+});
+
+test('run-vcpkg: basic run scenario test', async () => {
+    const vcpkg: vcpkgAction.VcpkgAction = new vcpkgAction.VcpkgAction(
+        new BaseUtilLib(new actionLib.ActionLib()));
+    await vcpkg.run();
+});
+
+test('run-vcpkg: cache hit scenario test', async () => {
+    // Arrange.
+    const saveCacheSpy = jest.spyOn(cache, "saveCache").mockImplementation(
+        function (a, b, c): Promise<number> { return Promise.resolve(17); });
+    const restoreCacheSpy = jest.spyOn(cache, "restoreCache").mockImplementation(
+        function (a, b, c): Promise<string> { return Promise.resolve("hit"); });
+    const keyMatchMock = jest.spyOn(vcpkgUtils, "isExactKeyMatch").mockImplementation(
+        function (key: string, cacheKey?: string): boolean {
+            return true;
+        });
+
+    // Act.
+    const vcpkg: vcpkgAction.VcpkgAction = new vcpkgAction.VcpkgAction(
+        new BaseUtilLib(new actionLib.ActionLib()));
+    await vcpkg.run();
+
+    // Asserts.
+    expect(restoreCacheSpy).toBeCalledTimes(1);
+    const key = restoreCacheSpy.mock.calls[0][1];
+    // Cache was hit, it must not be called!
+    expect(saveCacheSpy).toBeCalledTimes(0);
+
+    // Clear mocks.
+    keyMatchMock.mockRestore();
+});
+
+test('run-vcpkg: cache miss scenario test', async () => {
+    // Arrange.
+    const saveCacheSpy = jest.spyOn(cache, "saveCache").mockImplementation(
+        function (a, b, c): Promise<number> { return Promise.resolve(42); });
+    // Mock returns an underfined key since it is a cache miss.
+    const restoreCacheSpy = jest.spyOn(cache, "restoreCache").mockImplementation(
+        function (a, b, c): Promise<string | undefined> { return Promise.resolve(undefined); });
+    const keyMatchMock = jest.spyOn(vcpkgUtils, "isExactKeyMatch").mockImplementation(
+        function (key: string, cacheKey?: string): boolean {
+            return false;
+        });
+    const vcpkgRunnerRunSpy = jest.spyOn(runVcpkgLib.VcpkgRunner.prototype, "run");
+    // Act.
+    const vcpkg: vcpkgAction.VcpkgAction = new vcpkgAction.VcpkgAction(
+        new BaseUtilLib(new actionLib.ActionLib()));
+    await vcpkg.run();
+
+    // Asserts.
+    expect(restoreCacheSpy).toBeCalledTimes(1);
+    const key = restoreCacheSpy.mock.calls[0][1];
+    // Cache was missed, it must be called once!
+    expect(saveCacheSpy).toBeCalledTimes(1);
+    expect(vcpkgRunnerRunSpy).toBeCalledTimes(1);
+    expect(saveCacheSpy.mock.calls[0][1]).toBe(key);
+
+    // Clear mocks.
+    keyMatchMock.mockRestore();
+});
+
+test('run-vcpkg: cache must not be restored/saved when "doNotCache" is true scenario test', async () => {
+    // Arrange.
+    const saveCacheSpy = jest.spyOn(cache, "saveCache");
+    const restoreCacheSpy = jest.spyOn(cache, "restoreCache");
+    const keyMatchMock = jest.spyOn(vcpkgUtils, "isExactKeyMatch").mockImplementation(
+        function (key: string, cacheKey?: string): boolean {
+            return true;
+        });
+    const vcpkgRunnerRunSpy = jest.spyOn(runVcpkgLib.VcpkgRunner.prototype, "run");
+    jest.mock("@actions/core");
+    const inputMock = jest.spyOn(core, "getInput").mockImplementation(
+        (name: string, options): string => {
+            if (name === vcpkgAction.doNotCacheInput) {
+                return "true";
+            } else {
+                return jest.requireActual("@actions/core").getInput(name, options);
             }
-        );*/
+        })
 
-        process.env.GITHUB_WORKSPACE = "";
+    // Act.
+    const vcpkg: vcpkgAction.VcpkgAction = new vcpkgAction.VcpkgAction(
+        new BaseUtilLib(new actionLib.ActionLib()));
+    await vcpkg.run();
+
+    // Asserts.
+
+    // doNotCache is true, no cache operation must be carried.
+    expect(restoreCacheSpy).toBeCalledTimes(0);
+    expect(saveCacheSpy).toBeCalledTimes(0);
+    expect(vcpkgRunnerRunSpy).toBeCalledTimes(1);
+
+    // Clear mocks.
+    keyMatchMock.mockRestore();
+});
+
+test('isExactKeyMatch tests', () => {
+    const hash = vcpkgUtils.hashCode("hashCode");
+    const hash2 = vcpkgUtils.hashCode("hashCode2");
+    expect(vcpkgUtils.isExactKeyMatch(hash, hash2)).toBeFalsy();
+    expect(vcpkgUtils.isExactKeyMatch(hash2, hash)).toBeFalsy();
+    expect(vcpkgUtils.isExactKeyMatch(hash, hash)).toBeTruthy();
+});
+
+test('getVcpkgCommitId tests', async () => {
+    const p = path.resolve(path.join(__dirname, ".."));
+    const baseUtils = new BaseUtilLib(new ActionLib());
+    // It must return undefined when GITHUB_WORKSPACE is not defined.
+    process.env.GITHUB_WORKSPACE = undefined;
+    expect(await vcpkgUtils.getVcpkgCommitId(baseUtils, p)).toBe(undefined);
+
+    // It must return undefined when there is no vcpkg repository.
+    process.env.GITHUB_WORKSPACE = "/var/tmp/anything";
+    process.env.INPUT_VCPKGDIRECTORY = "fafsadfdsds";
+    expect(await vcpkgUtils.getVcpkgCommitId(baseUtils, p)).toBeFalsy();
+
+    const expectedVcpkgSubmoduleCommitId = "commit_id_sha";
+    // A commit SHA is 40 characters long.
+    jest.spyOn(fs, 'readFileSync').mockImplementationOnce(function (filePath) {
+        return expectedVcpkgSubmoduleCommitId;
     });
-
-    beforeEach(() => {
-        /*process.env[Events.Key] = Events.Push;
-        process.env[RefKey] = "refs/heads/feature-branch";*/
+    jest.spyOn(fs, 'existsSync').mockImplementationOnce(function (filePath) {
+        return true;
     });
-
-    afterEach(() => {
-        clearInputs();
-        /*delete process.env[Events.Key];
-        delete process.env[RefKey];*/
-    });
-
-    afterAll(async () => {
-        try {
-        } catch {
-            console.log('Failed to remove test directories');
-        }
-    });
-
-    test('basic cache hit scenario test', async () => {
-        //?? const vcpkg : action.VcpkgAction = new action.VcpkgAction(new actionLib.ActionLib());
-    });
-
-    test('basic cache miss scenario test', async () => {
-
-    });
+    const commitId = await vcpkgUtils.getVcpkgCommitId(baseUtils, p);
+    expect(commitId).toBe(expectedVcpkgSubmoduleCommitId);
 });
--- src/vcpkg-action.ts
+++ src/vcpkg-action.ts
@@ -2,143 +2,52 @@
 // Released under the term specified in file LICENSE.txt
 // SPDX short identifier: MIT
 
-import * as core from '@actions/core'
 import * as path from 'path'
-import * as fs from 'fs'
+import * as core from '@actions/core'
 import * as cache from '@actions/cache'
-import { BaseLib } from '@lukka/base-lib'
+import { BaseUtilLib } from '@lukka/base-util-lib'
 import * as runvcpkglib from '@lukka/run-vcpkg-lib'
+import { ensureDirExists, getVcpkgCommitId, hashCode, isExactKeyMatch } from './vcpkg-utils'
 
-// State's keys.
-export const VCPKGCACHEKEY = 'cacheKey';
-export const VCPKGCACHEHIT = 'cacheHit';
 // Input name for run-vcpkg only.
-export const doNotCache = 'doNotCache';
-
-function ensureDirExists(path: string): void {
-  try {
-    fs.mkdirSync(path, { recursive: true });
-  } catch (err) {
-    if (err.code !== 'EEXIST') {
-      core.warning(`Failed to create directory '${path}', error='${err}'.`);
-    }
-  }
-}
-
-/**
- * The input's name for additional content for the cache key.
- */
-export const appendedCacheKey = 'appendedCacheKey';
-
-export function getCachedPaths(): string[] {
-  const vcpkgDir = path.normalize(core.getInput(runvcpkglib.vcpkgDirectory));
-
-  ensureDirExists(vcpkgDir);
-  const pathsToCache: string[] = [
-    vcpkgDir,
-    path.normalize(`!${path.join(vcpkgDir, 'packages')}`),
-    path.normalize(`!${path.join(vcpkgDir, 'buildtrees')}`),
-    path.normalize(`!${path.join(vcpkgDir, 'downloads')}`)
-  ];
-  return pathsToCache;
-}
-
-/**
- * Compute an unique number given some text.
- * @param {string} text
- * @returns {string}
- */
-function hashCode(text: string): string {
-  let hash = 42;
-  if (text.length != 0) {
-    for (let i = 0; i < text.length; i++) {
-      const char: number = text.charCodeAt(i);
-      hash = ((hash << 5) + hash) ^ char;
-    }
-  }
-
-  return hash.toString();
-}
+export const doNotCacheInput = 'doNotCache';
 
 export class VcpkgAction {
-  constructor(private tl: BaseLib) {
+  /**
+   * The input's name for additional content for the cache key.
+   */
+  private readonly appendedCacheKey = 'appendedCacheKey';
+  private vcpkgCacheComputedKey: string | undefined;
+  private VCPKGCACHEHIT: string | undefined;
+
+  constructor(private baseUtilLib: BaseUtilLib) {
   }
 
   public async run(): Promise<void> {
-    core.startGroup('Restore vcpkg and its artifacts from cache');
-    try {
-      if (core.getInput(doNotCache).toLowerCase() === "true") {
-        core.info(`Caching is disabled (${doNotCache}=true)`);
-      } else {
-        // Get an unique output directory name from the URL.
-        const key: string = this.computeKey();
-        const pathsToCache: string[] = getCachedPaths();
-
-        core.info(`Cache's key = '${key}'.`);
-        core.saveState(VCPKGCACHEKEY, key);
-        core.info(`Running restore-cache`);
-
-        let cacheHitId: string | undefined;
-        try {
-          cacheHitId = await cache.restoreCache(pathsToCache, key);
-        }
-        catch (err) {
-          core.warning(`restoreCache() failed: '${err?.toString()}'.`)
-        }
-
-        if (cacheHitId) {
-          core.info(`Cache hit, id=${cacheHitId}.`);
-          core.saveState(VCPKGCACHEHIT, cacheHitId);
-        } else {
-          core.info(`Cache miss.`);
-        }
-      }
-    } finally {
-      core.endGroup()
-    }
-
-    const runner: runvcpkglib.VcpkgRunner = new runvcpkglib.VcpkgRunner(this.tl);
+    await this.baseUtilLib.wrapOp('Restore vcpkg and its artifacts from cache',
+      () => this.restoreCache());
+    const runner: runvcpkglib.VcpkgRunner = new runvcpkglib.VcpkgRunner(this.baseUtilLib.baseLib);
     await runner.run();
+    await this.baseUtilLib.wrapOp('Cache vcpkg and its artifacts', () => this.saveCache());
   }
 
-  private getVcpkgCommitId(): string {
-    let id = "";
-    const workspaceDir = process.env.GITHUB_WORKSPACE ?? "";
-    if (workspaceDir) {
-      let fullVcpkgPath = "";
-      const inputVcpkgPath = core.getInput(runvcpkglib.vcpkgDirectory);
-      if (path.isAbsolute(inputVcpkgPath))
-        fullVcpkgPath = path.normalize(path.resolve(inputVcpkgPath));
-      else
-        fullVcpkgPath = path.normalize(path.resolve(path.join(workspaceDir, inputVcpkgPath)));
-      core.debug(`fullVcpkgPath='${fullVcpkgPath}'`);
-      const relPath = fullVcpkgPath.replace(workspaceDir, '');
-      core.debug(`relPath='${relPath}'`);
-      const submodulePath = path.join(workspaceDir, ".git/modules", relPath, "HEAD")
-      core.debug(`submodulePath='${submodulePath}'`);
-      if (fs.existsSync(submodulePath)) {
-        id = fs.readFileSync(submodulePath).toString();
-        core.debug(`commitId='${id}'`);
-      }
-    }
-    return id.trim();
-  }
-
-  private computeKey(): string {
+  private async computeCacheKey(): Promise<string> {
     let key = "";
-    const commitId: string = this.getVcpkgCommitId();
+    const inputVcpkgPath = core.getInput(runvcpkglib.vcpkgDirectory);
+    const commitId: string | undefined = await getVcpkgCommitId(this.baseUtilLib, inputVcpkgPath);
+    const userProvidedCommitId = core.getInput(runvcpkglib.vcpkgCommitId);
     if (commitId) {
-      core.info(`vcpkg identified at commitId=${commitId}, adding it to the cache's key.`);
+      core.info(`vcpkg identified at commitId='${commitId}', adding it to the cache's key.`);
       key += `submodGitId=${commitId}`;
-    } else if (core.getInput(runvcpkglib.vcpkgCommitId)) {
-      key += "localGitId=" + hashCode(core.getInput(runvcpkglib.vcpkgCommitId));
+    } else if (userProvidedCommitId) {
+      key += "localGitId=" + hashCode(userProvidedCommitId);
     } else {
       core.info(`No vcpkg's commit id was provided, does not contribute to the cache's key.`);
     }
 
     key += "-args=" + hashCode(core.getInput(runvcpkglib.vcpkgArguments));
     key += "-os=" + hashCode(process.platform);
-    key += "-appendedKey=" + hashCode(core.getInput(appendedCacheKey));
+    key += "-appendedKey=" + hashCode(core.getInput(this.appendedCacheKey));
 
     // Add the triplet only if it is provided.
     const triplet = core.getInput(runvcpkglib.vcpkgTriplet)
@@ -146,4 +55,95 @@ export class VcpkgAction {
       key += "-triplet=" + hashCode(triplet);
     return key;
   }
+
+  private async saveCache(): Promise<void> {
+    try {
+      if (core.getInput(doNotCacheInput).toLowerCase() === "true") {
+        core.info(`Caching is disabled (${doNotCacheInput}=true)`);
+      } else {
+        if (!this.vcpkgCacheComputedKey) {
+          core.warning(`Error retrieving cache's key.`);
+          return;
+        }
+
+        if (isExactKeyMatch(this.vcpkgCacheComputedKey, this.VCPKGCACHEHIT)) {
+          core.info(`Cache hit occurred on the cache key '${this.vcpkgCacheComputedKey}', saving cache is skipped.`);
+          return;
+        } else {
+          const pathsToCache: string[] = this.getCachedPaths();
+          core.info(`Caching paths: '${pathsToCache}'`);
+          console.log(`Running save-cache`);
+
+          try {
+            await cache.saveCache(this.getCachedPaths(), this.vcpkgCacheComputedKey);
+          }
+          catch (error) {
+            if (error.name === cache.ValidationError.name) {
+              throw error;
+            } else if (error.name === cache.ReserveCacheError.name) {
+              core.info(error.message);
+            } else {
+              core.warning(error.message);
+            }
+          }
+        }
+      }
+      core.info('run-vcpkg post action execution succeeded');
+      process.exitCode = 0;
+    } catch (err) {
+      const errorAsString = (err ?? "undefined error").toString();
+      core.error(errorAsString);
+      core.setFailed('run-vcpkg post action execution failed');
+      process.exitCode = -1000;
+    }
+  }
+
+  private getCachedPaths(): string[] {
+    const vcpkgDir = path.normalize(core.getInput(runvcpkglib.vcpkgDirectory));
+
+    ensureDirExists(vcpkgDir);
+    const pathsToCache: string[] = [
+      vcpkgDir,
+      path.normalize(`!${path.join(vcpkgDir, 'packages')}`),
+      path.normalize(`!${path.join(vcpkgDir, 'buildtrees')}`),
+      path.normalize(`!${path.join(vcpkgDir, 'downloads')}`)
+    ];
+    return pathsToCache;
+  }
+
+  private async restoreCache(): Promise<void> {
+    if (core.getInput(doNotCacheInput).toLowerCase() === "true") {
+      core.info(`Caching is disabled (${doNotCacheInput}=true)`);
+    } else {
+      const key: string = await this.computeCacheKey();
+      const pathsToCache: string[] = this.getCachedPaths();
+
+      core.info(`Cache's key = '${key}'.`);
+      this.vcpkgCacheComputedKey = key;
+      core.info(`Running restore-cache`);
+
+      let cacheHitId: string | undefined;
+      try {
+        cacheHitId = await cache.restoreCache(pathsToCache, key);
+      }
+      catch (err) {
+        try {
+          core.warning(`restoreCache() failed once: '${err?.toString()}' , retrying...`);
+          cacheHitId = await cache.restoreCache(pathsToCache, key);
+        }
+        catch (err) {
+          core.warning(`restoreCache() failed again: '${err?.toString()}'.`);
+        }
+      }
+
+      if (cacheHitId) {
+        core.info(`Cache hit, id=${cacheHitId}.`);
+        this.VCPKGCACHEHIT = cacheHitId;
+      } else {
+        core.info(`Cache miss.`);
+      }
+    }
+  }
 }
+
+
